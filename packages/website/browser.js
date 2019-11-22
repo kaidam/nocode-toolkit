e5492575460cf53a53046e85ec56763b02723bbe
Kai Davenport <kaiyadavenport@gmail.com>
2019-11-22 21:00:17 +0000

    actually include the fix for dynamic paths

--- packages/website/browser.js
+++ packages/website/browser.js
@@ -1,3 +1,8 @@
+// dynamically set the webpack public path
+// so dynamically loaded modules load
+// with their website id in the url so the
+// server can figure out what template the website is using
+__webpack_public_path__ = window._nocodeBaseUrl
 import React from 'react'
 import { hydrate, render } from 'react-dom'
 import { Provider } from 'react-redux'
