3c2efdc219c223d1b1c0fbfd4501e78e4f9f8e45
Kai Davenport <kaiyadavenport@gmail.com>
2019-12-11 12:01:03 +0000

    schema for payment button

--- packages/plugin-stripe/ui/schema.js
+++ packages/plugin-stripe/ui/schema.js
@@ -8,12 +8,67 @@ const paymentButton = {
   metadata: {},
   parentFilter: ['cell'],
   initialValues: {
-    title: '',
+    name: '',
+    price: 0,
+    currency: '',
+    description: '',
   },
   schema: [{
-    id: 'title',
-    title: 'title',
-    helperText: 'Enter the text for the title',
+    id: 'name',
+    title: 'Product Name',
+    helperText: 'Enter the name of the product',
+    validate: {
+      type: 'string',
+      methods: [
+        ['required', 'The name is required'],
+      ],
+    }
+  },[{
+    id: 'price',
+    title: 'Price',
+    helperText: 'Enter the price for the product',
+    inputProps: {
+      type: 'number',
+    },
+    validate: {
+      type: 'number',
+      methods: [
+        ['required', 'The price is required'],
+      ],
+    }
+  },{
+    id: 'currency',
+    title: 'Currency',
+    helperText: 'Enter the currency',
+    component: 'select',
+    options: [{
+      title: 'GBP (£)',
+      value: 'GBP',
+    },{
+      title: 'USD ($)',
+      value: 'USD',
+    }],
+    validate: {
+      type: 'string',
+      methods: [
+        ['required', 'The currency is required'],
+      ],
+    }
+  }],{
+    id: 'buttonTitle',
+    title: 'Button Title',
+    helperText: 'Enter the title of the purchase button',
+  },{
+    id: 'description',
+    title: 'Description',
+    helperText: 'Enter a description for the product',
+    component: 'textarea',
+    validate: {
+      type: 'string',
+      methods: [
+        ['required', 'The description is required'],
+      ],
+    }
   }],
   cellConfig: {
     component: () => (<div>payment button</div>),
