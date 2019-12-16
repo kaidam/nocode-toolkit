5f0c8fb78c85fa6c578f448a44098f990d505128
Kai Davenport <kaiyadavenport@gmail.com>
2019-12-16 13:47:35 +0000

    update actions

--- packages/website/actions.js
+++ packages/website/actions.js
@@ -1,3 +1,4 @@
+// actions
 export { useDispatch } from 'react-redux'
 export { default as nocodeActions } from '@nocode-toolkit/website/store/moduleNocode'
 export { default as routerActions } from '@nocode-toolkit/website/store/moduleRouter'
