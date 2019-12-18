81f240bea31c99d114ea24fc223f7265ae1fb9f2
Kai Davenport <kaiyadavenport@gmail.com>
2019-12-18 19:59:19 +0000

    social links plugin rendering

--- packages/plugin-sociallinks/ui/SocialLinks.js
+++ packages/plugin-sociallinks/ui/SocialLinks.js
@@ -1,13 +1,60 @@
 import React from 'react'
 
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    display: 'inline-block',
+    flex: 1,
+    textAlign: 'center',
+  },
+  facebook: {
+    color: '#4166B2',
+  },
+  twitter: {
+    color: '#1995E0',
+  },
+  linkedin: {
+    color: '#0277B5',
+  },
+  youtube: {
+    color: '#FF0202',
+  },
+}
+
+const LINKS = [
+  'facebook',
+  'twitter',
+  'linkedin',
+  'youtube',
+]
+
 const SocialLinks = ({
   content,
 }) => {
-  console.log('--------------------------------------------')
-  console.dir(content)
+  const links = LINKS
+    .filter(name => content[name] ? true : fase)
+    .map((name, i) => {
+      const value = content[name]
+      return (
+        <div style={ styles.item } key={ i }>
+          <a href={ value }>
+            <span style={ styles[name] }>
+              <i className={`fab fa-${name} fa-2x`}></i>
+            </span>
+          </a>
+        </div>
+      )
+    })
   return (
-    <div>
-      Social Links
+    <div style={ styles.container }>
+      {
+        links
+      }
     </div>
   )
 }
