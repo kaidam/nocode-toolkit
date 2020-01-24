42dd899f2081a75405079aa9621b10bf57b04786
Kai Davenport <kaiyadavenport@gmail.com>
2020-01-24 16:45:37 +0000

    list files in alphabetical order

--- packages/ui/store/modules/finder.js
+++ packages/ui/store/modules/finder.js
@@ -32,6 +32,11 @@ const reducers = {
       driver,
       items,
     } = action.payload
+    items.sort((a, b) => {
+      const aName = a.name.toLowerCase()
+      const bName = b.name.toLowerCase()
+      return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+    })
     const driverSchema = library.get([driver, 'finder'].join('.'))
     const folders = items.filter(item => driverSchema.finder.isFolder(item))
     const nonFolders = items.filter(item => !driverSchema.finder.isFolder(item))
