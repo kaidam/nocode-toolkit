4f571f927f6e4ab63ad0c77f03f950eb7bd88e62
Kai Davenport <kaiyadavenport@gmail.com>
2020-03-13 13:11:38 +0000

    tab layout

--- packages/frontend/components/widgets/Tabs.js
+++ packages/frontend/components/widgets/Tabs.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import classnames from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
 
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  topbar: {
+    flexGrow: 0,
+  },
+  content: {
+    flexGrow: 0,
+  },
+  scroll: {
+    overflowY: 'auto',
+    overflowX: 'hidden',
+  },
+}))
 
 /*
 
@@ -17,26 +37,39 @@ import Tab from '@material-ui/core/Tab'
 const TabsWrapper = ({
   tabs,
   current,
+  autoScroll = true,
   onChange,
 }) => {
+
+  const classes = useStyles()
+
   if(!tabs || tabs.length <= 0) return null
   let currentTab = tabs.find(tab => tab.id == current)
   currentTab = currentTab || tabs[0]
+
+  const contentClassname = classnames({
+    [classes.content]: true,
+    [classes.scroll]: autoScroll,
+  })
   return (
-    <React.Fragment>
-      <Tabs value={ currentTab.id } onChange={ (e, value) => onChange(value) }>
+    <div className={ classes.container }>
+      <div className={ classes.topbar }>
+        <Tabs value={ currentTab.id } onChange={ (e, value) => onChange(value) }>
+          {
+            tabs.map((tab, i) => {
+              return (
+                <Tab key={ i } label={ tab.title } value={ tab.id } />
+              )
+            })
+          }
+        </Tabs>
+      </div>
+      <div className={ contentClassname }>
         {
-          tabs.map((tab, i) => {
-            return (
-              <Tab key={ i } label={ tab.title } value={ tab.id } />
-            )
-          })
+          currentTab.element
         }
-      </Tabs>
-      {
-        currentTab.element
-      }
-    </React.Fragment>
+      </div>
+    </div>
   )
   
 }
